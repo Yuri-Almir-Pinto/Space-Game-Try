cba31064030ef416f14e09605db95de2d775ada3
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2023-10-21 13:53:20 -0300

    Adicionado struct que centraliza os assets que o código utilizará.

---
+++ Game Try/Utils/Spriting/ESprites.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Try.Utils.Spriting
+{
+    public struct ESprites
+    {
+        public const string SPACESHIP_PATH = "Character/Spaceship";
+        public const string ALIEN_PATH = "Enemies/Alien";
+        public const string BACKGROUND_PATH = "Background/Space";
+    }
+}
